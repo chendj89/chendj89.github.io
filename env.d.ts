d7d8dba36c9cc9ad3077c719202bd4cf63892c15
chendj89 <chendj89@hotmail.com>
2022-12-24 12:46:48 +0800

    初始化项目

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+import './auto-imports';
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+/// <reference types="vite/client" />
---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import Unocss from 'unocss/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
+      imports: [
+        'vue',
+        'vue-router',
+        {
+          'naive-ui': ['n-card'],
+        },
+      ],
+      defaultExportByFilename: false,
+      dirs: ['./components'],
+      dts: './auto-imports.d.ts',
+      vueTemplate: false,
+      resolvers: [],
+      eslintrc: {
+        enabled: true,
+        filepath: './.eslintrc-auto-import.json',
+        globalsPropValue: true,
+      },
+    }),
+    Components({
+      dts: true,
+      resolvers: [NaiveUiResolver()],
+    }),
+    Unocss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
